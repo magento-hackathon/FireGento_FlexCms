ccaf0da4526e7b9dcf5087ff1d418e903b3e4c0f
Andreas von Studnitz <avs@integer-net.de>
2014-10-25 16:05:04 +0200

    Added unit tests for content links

--- src/app/code/community/Firegento/FlexCms/Test/Model/Content/Link.php
+++ src/app/code/community/Firegento/FlexCms/Test/Model/Content/Link.php
@@ -13,13 +13,13 @@ class Firegento_FlexCms_Test_Model_Content_Link extends EcomDev_PHPUnit_Test_Cas
      */
     public function testModelAndCollection()
     {
-        $content = Mage::getModel('firegento_flexcms/content_link')->load(1);
-        $this->assertEquals(1, $content->getId());
-        $this->assertEquals('default', $content->getLayoutHandle());
+        $contentLink = Mage::getModel('firegento_flexcms/content_link')->load(1);
+        $this->assertEquals(1, $contentLink->getId());
+        $this->assertEquals('default', $contentLink->getLayoutHandle());
 
-        /** @var $content Firegento_FlexCms_Model_Content */
-        $content = Mage::getModel('firegento_flexcms/content_link');
-        $content->addData(array(
+        /** @var $contentLink Firegento_FlexCms_Model_Content */
+        $contentLink = Mage::getModel('firegento_flexcms/content_link');
+        $contentLink->addData(array(
             'content_id' => 1,
             'layout_handle' => 'checkout_cart_index',
             'area' => 'contnent',
@@ -30,5 +30,24 @@ class Firegento_FlexCms_Test_Model_Content_Link extends EcomDev_PHPUnit_Test_Cas
         $contentCollection = Mage::getResourceModel('firegento_flexcms/content_link_collection');
         
         $this->assertEquals(2, $contentCollection->getSize());
+    }
+
+    /**
+     * @test
+     * @loadFixture ~Firegento_FlexCms/content
+     */
+    public function testDeleteContent()
+    {
+        $content = Mage::getModel('firegento_flexcms/content')->load(1);
+        
+        $content->delete();
+
+        $content = Mage::getModel('firegento_flexcms/content')->load(1);
+        
+        $this->assertNotEquals(1, $content->getId());
+
+        $contentLink = Mage::getModel('firegento_flexcms/content_link')->load(1);
+        
+        $this->assertNotEquals(1, $contentLink->getId());
     }
 }
