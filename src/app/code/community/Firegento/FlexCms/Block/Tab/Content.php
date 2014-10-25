49f6ba28ca94ec06ec7ad3080deb537650dc2cb4
Juan Alonso <juan.alonso@unic.com>
2014-10-25 15:22:53 +0200

    adding tab content for flex cms form

---
+++ src/app/code/community/Firegento/FlexCms/Block/Tab/Content.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Form.php
+ *
+ * @category  magento
+ * @package   magento_
+ * @copyright Copyright (c) 2014 Unic AG (http://www.unic.com)
+ * @author    juan.alonso@unic.com
+ */
+
+class Firegento_FlexCms_Block_Tab_Content extends Mage_Adminhtml_Block_Widget_Form
+{
+
+
+
+
+}
