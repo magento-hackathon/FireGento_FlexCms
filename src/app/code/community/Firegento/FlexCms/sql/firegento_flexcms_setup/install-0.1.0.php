e8d322f4c76a8eb6a2ff33351d58e93f9be7e280
Andreas von Studnitz <avs@integer-net.de>
2014-10-25 13:53:28 +0200

    Added first database table

---
+++ src/app/code/community/Firegento/FlexCms/sql/firegento_flexcms_setup/install-0.1.0.php
@@ -0,0 +1,40 @@
+<?php
+$installer = $this;
+/* @var $installer Mage_Core_Model_Resource_Setup */
+
+$installer->startSetup();
+
+/**
+ * Create table 'firegento_flexcms/content'
+ */
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('firegento_flexcms/content'))
+    ->addColumn('flexcms_content_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'identity'  => true,
+        'unsigned'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+        ), 'Content Id')
+    ->addColumn('title', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+        'nullable'  => false,
+        'default'   => '',
+        ), 'Title')
+    ->addColumn('content', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
+        'nullable'  => false,
+        'default'   => '',
+        ), 'Title')
+    ->addColumn('content_type', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+        'nullable'  => false,
+        'default'   => '',
+    ), 'Content Type')
+    ->addColumn('created_at', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
+        'nullable'  => false,
+    ), 'Created At')
+    ->addColumn('updated_at', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
+        'nullable'  => false,
+    ), 'Created At')
+    ->setComment('FlexCms Content');
+
+$installer->getConnection()->createTable($table);
+
+$installer->endSetup();
