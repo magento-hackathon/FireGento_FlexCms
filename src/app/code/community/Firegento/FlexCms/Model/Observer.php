25a0393dfd8d9c25b6360f1166aced281025ae1d
Michael Scheel <scheel@virtualways.de>
2014-10-25 14:12:27 +0200

    Adding Layout Handle for CMS-Pages via Observer

---
+++ src/app/code/community/Firegento/FlexCms/Model/Observer.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of a FireGento e.V. module.
+ *
+ * This FireGento e.V. module is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category  FireGento
+ * @package   FireGento_FlexCms
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2014 FireGento Team (http://www.firegento.com)
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ */
+
+/**
+ * FlexCms Observer
+ *
+ * @category FireGento
+ * @package  FireGento_ContentSync
+ * @author   FireGento Team <team@firegento.com>
+ */
+class Firegento_FlexCms_Model_Observer
+{
+    /**
+     * add CMSPAGE_{ID} handle to layout on cms page render
+     *
+     * @param Varien_Event_Observer $observer
+     * @return Varien_Event_Observer
+     */
+    public function cmsPageRender(Varien_Event_Observer $observer)
+    {
+        $event = $observer->getEvent();
+
+        $event->getControllerAction()->getLayout()->getUpdate()
+            ->addHandle('CMSPAGE_'.$event->getPage()->getId());
+
+        return $observer;
+    }
+}
