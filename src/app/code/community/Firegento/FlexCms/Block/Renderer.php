7c9cfff0125850f89f7edc3aa817f779a273aaf8
Michael Scheel <scheel@virtualways.de>
2014-10-25 17:43:43 +0200

    Content Element Frontend Renderer

--- src/app/code/community/Firegento/FlexCms/Block/Renderer.php
+++ src/app/code/community/Firegento/FlexCms/Block/Renderer.php
@@ -34,34 +34,97 @@ class Firegento_FlexCms_Block_Renderer extends Mage_Core_Block_Template
     protected $_template = "firegento/flexcms/area.phtml";
 
     /**
-     *
+     * @var array
+     */
+    protected $_layoutHandles = array('default');
+
+    /* @var Firegento_FlexCms_Model_Resource_Content_Link_Collection */
+    protected $_contentElements = array();
+
+    /**
+     * collect all layout handles
      */
     protected function _construct()
     {
-        $this->_loadContentElements();
-
-        parent::_construct();
+        $this->_layoutHandles = Mage::app()->getLayout()->getUpdate()->getHandles();
     }
 
     /**
-     *
+     * load content elements
+     */
+    protected function _beforeToHtml()
+    {
+        $this->_loadContentElements();
+    }
+
+    /**
+     * load content elements by sort order
      */
     private function _loadContentElements()
     {
+        /* @var Firegento_FlexCms_Model_Resource_Content_Link_Collection $linkCollection */
+        $linkCollection = $this->getContentCollection();
 
-        /* @var array $layoutHandles */
-        $layoutHandles = Mage::app()->getLayout()->getUpdate()->getHandles();
+        $layoutHandles = array_flip($this->_layoutHandles);
 
-        /* @var Firegento_FlexCms_Model_Resource_Content_Link_Collection $contentCollection */
-        $contentCollection = Mage::getModel('firegento_flexcms/content_link')->getCollection()
+        // determine content element for sort orders, keep most important element per order
+        foreach ($linkCollection as $link) {
+            $order = $link->getSortOrder();
+            if (isset($this->_contentElements[$order])) {
+                if ($layoutHandles[$this->_contentElements[$order]["layout_handle"]] < $layoutHandles[$link["layout_handle"]]) {
+                    $this->_contentElements[$order] = $link;
+                }
+                break;
+            }
+
+            $this->_contentElements[$order] = $link;
+        }
+
+        // sort content elements by sort order
+        ksort($this->_contentElements);
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function setContentElements($value)
+    {
+        $this->_contentElements = $value;
+        return $this;
+    }
+
+    /**
+     * @return Firegento_FlexCms_Model_Resource_Content_Link_Collection
+     */
+    public function getContentElements()
+    {
+        return $this->_contentElements;
+    }
+
+    /**
+     * fetch content links, filtered by current store, area and layout_handle
+     *
+     * @return Firegento_FlexCms_Model_Resource_Content_Link_Collection
+     */
+    private function getContentCollection()
+    {
+        $linkCollection = Mage::getModel('firegento_flexcms/content_link')->getCollection()
             ->addFieldToFilter('area', array('eq' => $this->getAreaKey()))
-            ->addFieldToFilter('layout_handle', array('in' => $layoutHandles));
+            ->addFieldToFilter('layout_handle', array('in' => $this->_layoutHandles));
 
         if (!Mage::app()->isSingleStoreMode()) {
-            $contentCollection->addFieldToFilter('store_id',
+            $linkCollection->addFieldToFilter('store_id',
                 array('finset' => array(0, Mage::app()->getStore()->getId()))
             );
         }
 
+        $linkCollection->getSelect()->join(array(
+                'content' => Mage::getSingleton('core/resource')->getTableName('firegento_flexcms/content')),
+            'main_table.content_id = content.flexcms_content_id',
+            array('content.*')
+        );
+
+        return $linkCollection;
     }
 }
