846c7e81cc45eabeec15fe6263f0d802f7a7a286
Juan Alonso <juan.alonso@unic.com>
2014-10-25 18:40:11 +0200

    Load existing flexcms content for category

--- src/app/code/community/Firegento/FlexCms/Block/Tab/Content.php
+++ src/app/code/community/Firegento/FlexCms/Block/Tab/Content.php
@@ -26,20 +26,38 @@ class Firegento_FlexCms_Block_Tab_Content extends Mage_Adminhtml_Block_Widget_Fo
 
             $fieldset = $form->addFieldset($section['key'], array('legend' => Mage::helper('firegento_flexcms')->__($section['title'])));
 
+            $layoutHandle = 'Category_'.$this->getCategoryId();
+            $element = Mage::getModel('firegento_flexcms/content_link')->loadByHandleAndArea($layoutHandle, $section['key']);
+
             $fieldset->addField('content_element_'.$section['key'], 'select', array(
                     'label' => Mage::helper('firegento_flexcms')->__('Content Element'),
                     'name' => 'flexcms_element['.$section['key'].'][content_id]',
-                    'values' => Mage::helper('firegento_flexcms')->getFlexContents()
+                    'values' => Mage::helper('firegento_flexcms')->getFlexContents(),
+                    'value' => ($element->getId()) ? $element->getContentId() : 0
                 ));
 
             $fieldset->addField('sort_order'.$section['key'], 'text', array(
                     'label' => Mage::helper('firegento_flexcms')->__('Sort Order'),
-                    'name' => 'flexcms_element['.$section['key'].'][sort_order]'
+                    'name' => 'flexcms_element['.$section['key'].'][sort_order]',
+                    'value' => ($element->getId()) ? $element->getSortOrder() : ""
                 ));
 
         }
 
         return parent::_prepareForm();
+    }
+
+    /**
+     * Retrieve current category instance
+     *
+     * @return Mage_Catalog_Model_Category
+     */
+    public function getCategoryId()
+    {
+        if (Mage::registry('category')) {
+            return Mage::registry('category')->getId();
+        }
+        return false;
     }
 
 }
