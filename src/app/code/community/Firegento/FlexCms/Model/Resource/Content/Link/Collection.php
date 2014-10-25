67e7dc87f828e854e99cca14f13808a442c68990
Michael Scheel <scheel@virtualways.de>
2014-10-25 22:30:13 +0200

    Refactored Content Update Grid / Moved content table join in resource collection

--- src/app/code/community/Firegento/FlexCms/Model/Resource/Content/Link/Collection.php
+++ src/app/code/community/Firegento/FlexCms/Model/Resource/Content/Link/Collection.php
@@ -10,4 +10,15 @@ class Firegento_FlexCms_Model_Resource_Content_Link_Collection extends Mage_Core
     {
         $this->_init('firegento_flexcms/content_link');
     }
+
+    /**
+     * join content from firegento_flexcms/content 1:1
+     */
+    public function joinContentData(){
+        $this->getSelect()->join(
+            array('reference_table' => Mage::getSingleton('core/resource')->getTableName('firegento_flexcms/content')),
+            'reference_table.flexcms_content_id=main_table.content_id',
+            array('reference_table.*')
+        );
+    }
 }
