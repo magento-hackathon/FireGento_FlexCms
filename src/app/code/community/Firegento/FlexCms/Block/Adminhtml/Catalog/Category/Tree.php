bae66bcc8084c22cac729c1f8e6d5b25626f8fe0
Andreas von Studnitz <avs@integer-net.de>
2015-05-27 12:36:02 +0200

    Mark categories with changes blue in category tree

---
+++ src/app/code/community/Firegento/FlexCms/Block/Adminhtml/Catalog/Category/Tree.php
@@ -0,0 +1,56 @@
+<?php
+class Firegento_FlexCms_Block_Adminhtml_Catalog_Category_Tree extends Mage_Adminhtml_Block_Catalog_Category_Tree
+{
+    protected function _getNodeJson($node, $level = 0)
+    {
+        // create a node from data array
+        if (is_array($node)) {
+            $node = new Varien_Data_Tree_Node($node, 'entity_id', new Varien_Data_Tree);
+        }
+
+        $item = array();
+        $item['text'] = $this->buildNodeName($node);
+
+        /* $rootForStores = Mage::getModel('core/store')
+            ->getCollection()
+            ->loadByCategoryIds(array($node->getEntityId())); */
+        $rootForStores = in_array($node->getEntityId(), $this->getRootIds());
+
+        $item['id']  = $node->getId();
+        $item['store']  = (int) $this->getStore()->getId();
+        $item['path'] = $node->getData('path');
+
+        $item['cls'] = 'folder ' . ($node->getIsActive() ? 'active-category' : 'no-active-category');
+
+        if (Mage::helper('firegento_flexcms')->getChangesObject($node)->getId()) {
+            $item['cls'] .= ' has-workflow-changes';
+        }
+
+        //$item['allowDrop'] = ($level<3) ? true : false;
+        $allowMove = $this->_isCategoryMoveable($node);
+        $item['allowDrop'] = $allowMove;
+        // disallow drag if it's first level and category is root of a store
+        $item['allowDrag'] = $allowMove && (($node->getLevel()==1 && $rootForStores) ? false : true);
+
+        if ((int)$node->getChildrenCount()>0) {
+            $item['children'] = array();
+        }
+
+        $isParent = $this->_isParentSelectedCategory($node);
+
+        if ($node->hasChildren()) {
+            $item['children'] = array();
+            if (!($this->getUseAjax() && $node->getLevel() > 1 && !$isParent)) {
+                foreach ($node->getChildren() as $child) {
+                    $item['children'][] = $this->_getNodeJson($child, $level+1);
+                }
+            }
+        }
+
+        if ($isParent || $node->getLevel() < 2) {
+            $item['expanded'] = true;
+        }
+
+        return $item;
+    }
+}
